7d7b4752950d3b81fa2ee1c48d186e100d75d431
juanfonsecadev <juanof2000@gmail.com>
2023-06-05 16:21:57 -0300

    Terminei a resolução do código com Js e resolvi alguns problemas enfrentados. Agora está funcionando perfeitamente.

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
 let checklist = document.querySelector("#checklist").addEventListener('click', function(click){ 
 
-    if(click.target.tagName === "INPUT"){ 
+    if(click.target.tagName === "INPUT" && click.target.type === "checkbox"){ 
 
         let listItem = click.target.parentNode;
 
         listItem.classList.toggle("checked");
     }
+}); //faz com que o texto receba o estilo de "checked" quando marcado.
+
+
+
+let button = document.getElementById("add").addEventListener('click', function(){ 
+
+    //console.log('Fui clicado!')
+
+    let checklist = document.querySelector("#checklist"); //pega a ul checklist pelo id
+    
+    let itemNovo = document.createElement('li'); //cria um elemento <li> no documento
+
+    let novoCheck = document.createElement('input'); //cria um input
+    novoCheck.type = "checkbox"; //transforma esse input em "checkbox"
+
+    var novoTexto = document.createElement('input'); //cria um input
+    novoTexto.type = "text"; //transforma esse input em "text"
+    novoTexto.placeholder = "Adicione um novo item."; //adiciona um placeholder
+    novoTexto.id = "item-lista"; //transforma o id em "item-lista"
+
+    let botaoDelete = document.createElement('button');
+    botaoDelete.className = "del-button";
+    botaoDelete.textContent = "X";
+
+
+    itemNovo.appendChild(novoCheck);
+    itemNovo.appendChild(novoTexto);
+    itemNovo.appendChild(botaoDelete);
+    checklist.appendChild(itemNovo);
+
+    botaoDelete.addEventListener('click', function(){ 
+
+        let itemLista = this.parentNode; 
+
+        itemLista.parentNode.removeChild(itemLista)
+    });
+
+
+    //console.log('estou ok')
 });
+
 
 
 
